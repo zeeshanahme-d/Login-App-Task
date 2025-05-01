a2a6b981930545ce3089b4eb820dc7be139c142a
Zeeshan Ahmed <dev.zeeshanahmed@gmail.com>
2025-05-01 02:49:55 -0700

    refactor: simplify ProtectedRoute component by renaming WrappedComponent

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,18 +2,16 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-const ProtectedRoute = <P extends object>(WrappedComponent: React.ComponentType<P>) => {
-  const ComponentWithAuth = (props: P) => {
+const ProtectedRoute = (Component: React.ComponentType) => {
+  return function WrappedComponent(props: any) {
     const { user } = useAuth();
 
     if (!user) {
       return <Navigate to="/" replace />;
     }
 
-    return <WrappedComponent {...props} />;
+    return <Component {...props} />;
   };
-
-  return ComponentWithAuth;
 };
 
 export default ProtectedRoute;
